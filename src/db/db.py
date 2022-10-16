f51cafd0482fcddf793f7a148044f72d9edc2a74
Patrick Applegate <papplega@gmail.com>
2022-10-15 22:01:09 -0400

    initial database material

--- src/db/db.py
+++ src/db/db.py
@@ -1,16 +1,14 @@
+from dataclasses import dataclass
 from sqlite3 import connect
 from os.path import exists
 
 from ..defs.settings import Settings
 
 
-def create_db() -> None:
+@dataclass(frozen=True)
+class Queries:
 
-    connection = connect(Settings.db_path)
-    cursor = connection.cursor()
-
-    cursor.execute(
-        """
+    create_table: str = """
         create table widgets (
             uuid text primary key not null,
             name text not null,
@@ -19,12 +17,21 @@ def create_db() -> None:
             updated date
         );
     """
-    )
 
 
-def do_sql(query: str) -> None:
-    pass
+def do_sql(query: str, create_if_missing: bool=True) -> None:
+    """
+    Executes an SQL query.  if the database file doesn't exist, it will be
+    created, together with the widgets table.
+    """
+
+    if create_if_missing and not exists(Settings.db_path):
+        do_sql(Queries.create_table, False)
+
+    with connect(Settings.db_path) as connection:
+        cursor = connection.cursor()
+        cursor.execute(query)
 
 
 if __name__ == "__main__":
-    create_db()
+    do_sql(Queries.create_table, False)
