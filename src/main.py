397d8235143fadea02edf2b14adec7b825e4c97f
Patrick Applegate <papplega@gmail.com>
2022-10-18 12:56:48 -0400

    api works!

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
-from datetime import datetime, timezone
+from datetime import (
+    datetime,
+    timezone,
+)
 from json import loads
 from typing import (
-    cast,
     Dict,
     List,
     Mapping,
     Optional,
     Type,
+    cast,
 )
 from urllib.parse import parse_qsl
+from uuid import uuid4
 
 from tornado.ioloop import IOLoop
 from tornado.web import (
@@ -26,10 +30,11 @@ from .defs.widget import Widget
 
 
 class WidgetHandler(RequestHandler):
-    def get(self, params: Optional[str] = None):
+    def get(self):
         """
         Gets records from the database.  The specific behavior of this function
-        depends on params.
+        depends on the params passed through the request body (the data
+        argument in requests.get()).
         If params is None, all records are retrieved.
         If params contains a UUID, only the relevant record is retrieved.
         If params contains a name, only the relevant records are retrieved.
@@ -37,50 +42,49 @@ class WidgetHandler(RequestHandler):
         UUIDs and names beyond the first supplied are also ignored.
         """
         # TODO: add pagination
+        params = parse_arguments(self.request.body_arguments)
+        uuid = params.get("uuid")
+        name = params.get("name")
 
-        if params is None:
+        if uuid is not None and name is not None:
+            raise ValueError("Both uuid and name should not be supplied!")
+
+        elif uuid is None and name is None:
             db_widgets = do_sql(
                 Queries.select_all,
                 None,
                 Settings.database_path,
             )
+
+        elif uuid is not None:
+            db_widgets = do_sql(
+                Queries.select_by_uuid,
+                (uuid,),
+                Settings.database_path,
+            )
+
         else:
-            params_dict = parse(params)
-            uuid = params_dict.get("uuid")
-            name = params_dict.get("name")
-
-            neither = uuid is None and name is None
-            both = uuid is not None and name is not None
-            if neither or both:
-                raise ValueError("params must include either uuid or name!")
-
-            if uuid is not None:
-                db_widgets = do_sql(
-                    Queries.select_by_uuid,
-                    (uuid,),
-                    Settings.database_path,
-                )
-            else:
-                db_widgets = do_sql(
-                    Queries.select_by_name,
-                    (cast(str, name),),
-                    Settings.database_path,
-                )
+            db_widgets = do_sql(
+                Queries.select_by_name,
+                (cast(str, name),),
+                Settings.database_path,
+            )
 
         self.write({"widgets": to_array(db_widgets)})
 
-    def post(self, params):
+    def post(self):
         """
         Inserts a record into the database, given the values supplied in
         params.  params should include values for the name and parts fields
         of a Widget object; other fields will be ignored.
         """
-        params_dict = parse(params)
+        params = parse_arguments(self.request.body_arguments)
+
         try:
             new = Widget(
                 uuid=uuid4(),
-                name=params_dict.get("name"),
-                parts=int(params_dict.get("parts")),
+                name=params.get("name"),
+                parts=int(params.get("parts")),
                 created=datetime.now(timezone.utc),
                 updated=datetime.now(timezone.utc),
             )
@@ -88,6 +92,7 @@ class WidgetHandler(RequestHandler):
             raise ValueError(
                 "Bad parameters supplied!"
             ) from e  # TODO: fix errors -- emit to user, not server
+
         db_widgets = do_sql(
             Queries.insert_record,
             new.to_tuple(),
@@ -96,7 +101,7 @@ class WidgetHandler(RequestHandler):
 
         self.write({"widgets": to_array(db_widgets)})
 
-    def patch(self, params):
+    def patch(self):
         """
         Updates an existing record, given the values supplied in params.
         params should include a UUID that corresponds to a record in the
@@ -105,16 +110,16 @@ class WidgetHandler(RequestHandler):
         can be directly changed by this route; the uuid and created fields are
         fixed once set, and the updated field is changed automatically.
         """
-        params_dict = dict(parse_qsl(params))
-        to_update = {
-            k: v for k, v in params_dict.items() if k in ("name", "parts")
-        }
+        params = parse_arguments(self.request.body_arguments)
+        to_update = {k: v for k, v in params.items() if k in ("name", "parts")}
         uuid = params["uuid"]  # TODO: can get an error here
+
         db_widgets = do_sql(
             Queries.select_by_uuid,
             (uuid,),
             Settings.database_path,
         )  # TODO: another error if db_widgets is empty
+
         old = Widget.from_tuple(db_widgets[0])
         new = replace(
             old_widget, **to_update, updated=datetime.now(timezone.utc)
@@ -128,28 +133,29 @@ class WidgetHandler(RequestHandler):
 
         self.write({"widgets": to_array(db_widgets)})
 
-    def delete(self, params):
+    def delete(self):
         """
         Deletes a record from the database with a UUID supplied in params.
         Other fields supplied through params will be ignored.  Sends back an
         empty array if successful, indicating that the corresponding record
         no longer exists in the database.
         """
-        params_dict = parse(params)
+        params = parse_arguments(self.request.body_arguments)
+
         db_widgets = do_sql(
             Queries.delete_by_uuid,
-            (params_dict["uuid"],),  # TODO: can get an error here
+            (params["uuid"],),  # TODO: can get an error here
             Settings.database_path,
         )
 
         self.write({"widgets": to_array(db_widgets)})
 
 
-def parse(api_params: str) -> Dict[str, str]:
+def parse_arguments(arguments: Dict[str, List[bytes]]) -> Dict[str, str]:
     """
     Unpacks a string of params supplied to an API route to a dictionary.
     """
-    return dict(parse_qsl(api_params))
+    return {k: bytes.decode(v[0]) for k, v in arguments.items()}
 
 
 def to_array(widget_tuples: List[DbValues]) -> List[Dict[str, str]]:
@@ -157,7 +163,6 @@ def to_array(widget_tuples: List[DbValues]) -> List[Dict[str, str]]:
     Convenience function to unpack a list of tuples representing widgets to
     a list of dictionaries.
     """
-
     array = []
     for wt in widget_tuples:
         try:
@@ -169,7 +174,7 @@ def to_array(widget_tuples: List[DbValues]) -> List[Dict[str, str]]:
 
 
 handlers: Mapping[str, Type[WidgetHandler]] = {
-    r"/api/widgets/([^/]+)?": WidgetHandler,
+    "/api/widgets/": WidgetHandler,
 }
 
 if __name__ == "__main__":
